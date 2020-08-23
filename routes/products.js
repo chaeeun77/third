f5f000ad1c868f76fea97a3847f5989876c27e30
chaeeun <chaeeun77@naver.com>
2020-08-23 14:16:26 +0900

    create product to Mongo DB

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const productModel = require("../models/product")
 
 //product data 불러오기
 router.get('/total', (req, res) => {
@@ -10,15 +11,33 @@ router.get('/total', (req, res) => {
 
 //product data 생성하기
 router.post('/register', (req, res) => {
-    const product = {
+    // const product = {
+    //     name: req.body.productname,
+    //     price: req.body.productprice
+    // }
+    //
+    // res.json({
+    //     message: 'product data 생성하기',
+    //     createdProduct: product
+    // })
+    const newProduct = new productModel({
         name: req.body.productname,
         price: req.body.productprice
-    }
-
-    res.json({
-        message: 'product data 생성하기',
-        createdProduct: product
     })
+
+    newProduct
+        .save()
+        .then(doc => {
+            res.json({
+                message: "saved data",
+                productInfo: doc
+            })
+        })
+        .catch(err => {
+            res.json({
+                message: err.message
+            })
+        })
 })
 
 //product data 업데이트하기
