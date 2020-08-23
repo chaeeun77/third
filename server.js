e078641473b01f4a729f32a770dda6b4c077a4a7
chaeeun <chaeeun77@naver.com>
2020-08-23 11:17:30 +0900

    request response test

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res) => {
+    res.json({
+        message: 'It works!'
+    })
+})
+
 const PORT = 5000;
 app.listen(PORT, console.log('server start'));
