0b0b324e5a6ccf28896e7d5ac7885de8b09c35ae
chaeeun <chaeeun77@naver.com>
2020-08-23 12:19:33 +0900

    add morgan

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
-const productRoutes = require('./routes/products')
-const orderRoutes = require('./routes/orders')
+const productRoutes = require('./routes/products');
+const orderRoutes = require('./routes/orders');
+const morgan = require('morgan');
 // app.use((req, res) => {
 //     res.json({
 //         message: 'It works!'
 //     })
 // })
 
+//미들웨어 설정
+app.use(morgan('dev'));
+
+//router
 app.use('/products', productRoutes)
 app.use('/orders', orderRoutes)
 
