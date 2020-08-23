a037afe3bfd20bb07abf9bd4271e50fa27007e27
chaeeun <chaeeun77@naver.com>
2020-08-23 13:23:41 +0900

    mongoDb connect

--- server.js
+++ server.js
@@ -4,12 +4,23 @@ const productRoutes = require('./routes/products');
 const orderRoutes = require('./routes/orders');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 // app.use((req, res) => {
 //     res.json({
 //         message: 'It works!'
 //     })
 // })
 
+//database 연결
+const dbaddress = "mongodb+srv://third:codms13579@cluster0.loeyr.mongodb.net/shoppingmall?retryWrites=true&w=majority"
+const dboptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+mongoose
+    .connect(dbaddress, dboptions)
+    .then(() => console.log("MongoDb connected"))
+    .catch(err => console.log(err.message))
 //미들웨어 설정
 app.use(morgan('dev'));
 
